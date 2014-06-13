d95a4abe85b7830a683181ec5930c05c606a5e5f
Samuel <myhonor16@users.noreply.github.com>
2014-06-13 12:12:35 -0400

    Revert "inital commit"
    This reverts commit 2f168cd91e5978d1e070c9e629bd21caa0863d6b.

--- js/util/fouc.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('.fouc-loading').css('padding', '0');
-	$('.fouc-loading').css('display', 'none');
-
-	$('.fouc-content').css('display', 'block');
-
-	console.log('FOUC Script Successful');
-});
